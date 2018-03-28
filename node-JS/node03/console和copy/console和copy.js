3bb46b07cd59852754d972e7ed0a83c9f30594c7
李俊钦 <597232598@qq.com>
2018-03-28 21:19:46 +0800

    [提交者]李俊钦 [提交时间]2018.3.28 [提交内容]3.28的作业

---
+++ node-JS/node03/console和copy/console和copy.js
@@ -0,0 +1,17 @@
+/*
+ * console 用于提供控制台标准输出,
+ */
+// console对象有一下方法
+// console.log();
+// console.info();
+// console.error();
+// console.warn();
+// console.dir();
+// console.time();
+// console.timeEnd();
+// console.trace();
+// console.assert();
+//
+// var fs = require("fs");
+// fs.copyFileSync(src,dest,flags);
+// fs.copyFile(src,dest,flags,callback);
---
+++ node-JS/node03/复制文件模块&console/复制文件模块.js
@@ -0,0 +1,6 @@
+// 模块文件 nodeThird
+
+var nodeThird = require("nodeThird");
+
+nodeThird.copySync("./aa.txt","./bb.txt");
+nodeThird.copy("./cc.txt","./dd.txt");
